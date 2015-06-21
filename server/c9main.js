a4b447c8b6cdcd229d7178d83a6267df0d7e1381
mtu2005 <mtu2005@walla.com>
2015-06-21 23:27:59 +0000

    adding a cloud9 version of main.js

---
+++ server/c9main.js
@@ -0,0 +1,28 @@
+var router 	= require("./router");
+var http		= require("http");
+
+function start(route){
+  function onRequest(request, response){
+    var postData = "";
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(chunk) {
+      postData+= chunk;
+    });
+
+    request.addListener("end", function() {
+      route(request, response, postData);
+    });
+  }
+
+  var server = http.createServer(onRequest)
+  server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    var addr = server.address();
+    console.log("Chat server listening at", addr.address + ":" + addr.port);
+  });
+  
+}
+
+
+start(router.route);
