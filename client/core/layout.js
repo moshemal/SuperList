73ecb4dab99589b084e7b321875c7ade9fccf9dc
galiii <gali.brill.1408@gmail.com>
2015-08-13 15:15:41 +0300

    layout changes

--- client/core/layout.js
+++ client/core/layout.js
@@ -6,24 +6,31 @@ define(['jquery', 'text!./templates/l2U.html', 'text!./templates/l3W.html', 'ken
 	function getJLayout (type){
 		var layoutHtml;
 		switch (type){
-			case "2U":  layoutHtml = $(l2U);
+			case "2U":  layoutHtml = $(l2U); //
 									break;
-			case "3W":  layoutHtml = $(l3W);
+			case "3W":  layoutHtml = $(l3W); //
 									break;
 			default:    layoutHtml = $(l3W);
 		}
 		return $(layoutHtml);
 	}
 
-	function createLayout (type, selector){
-		var jLayout = getJLayout(type);
-		var container = $(selector);
+	function createLayout (type, selector){ //for now its "3W", "#container"
+		var jLayout = getJLayout(type); //will return l3W.html
+		var container = $(selector); //"#container"
 		container.css({
 			width: "100%",
 			height: "100%"
 		});
 		jLayout.appendTo(selector);
-		jLayout.kendoSplitter();
+		
+		//the craetion of kendo from libery vendours of kendoSplitter
+		jLayout.kendoSplitter({
+		panes: [
+               { collapsible: true, size: "250px" },//123
+               { collapsible: false },//4
+               { collapsible: true, size: "220px"}//,resizable: true }//5
+                ]}); 
 	}
 
 	return {
