44ced1fff8e41d8b1d89b4ddd46adb6696135961
galiii <gali.brill.1408@gmail.com>
2015-09-08 23:30:34 +0300

    Jason

--- server/db.js
+++ server/db.js
@@ -10,22 +10,29 @@ function createUser (user, properites){
 			return;
 		} 
 		else {
-			fs.mkdir('./db/' + user, //file db --> file user
+			fs.mkdir('./db/' + user , //file db --> file user
 			
 			function(){
 					fs.mkdir('./db/' + user + '/lists', //file db --> file user --> file list  
 					
 					
-					function(){//file db -->file user --> properties.json
-					
+					function(){
+					//file db -->file user --> properties.json
 				        //properites={
 						//"full": "value",
                         //	"job: "value"					
 						//}
+						var e =[];
+						
 						fs.writeFile('./db/' + user + '/properites.json', properites , 'utf8');
+						fs.writeFile('./db/' + user + '/lists/list.json', [] , 'utf8');
 						
 					});
+					
 			});
+		
+		
+		
 		}//end else
 	})
 }
