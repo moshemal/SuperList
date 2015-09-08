85e70010b7b0b78f3d10d8a3522eff57035cffd0
galiii <gali.brill.1408@gmail.com>
2015-09-08 23:31:42 +0300

    fo Jason

--- client/main.js
+++ client/main.js
@@ -2,18 +2,18 @@
  * Created by moshemal.
  */
 
-define(['jquery', 'modules/Login/Login', 'core/cookies', 'core/layout','core/windowcreate','core/request'],//'modules/ListView/ListView','core/list' ], 
-  function($, Login, cookies, layout, window,request){
+define(['jquery', 'modules/Login/Login', 'core/cookies',
+ 'core/layout','modules/Create/Create','core/windowcreate','core/request'],//'modules/ListView/ListView','core/list' ], 
+  function($, Login, cookies, layout,Create,window,request){
   'use strict';
   //global vars
   var AUTH_STR = "auth";
 	var login;
-	//var create;
-	var user;
+	var create;
+	var user = null;
 	var lst;
 //for User exsiset	
   function startLoggin(){
-    
 	function loginSuccess(){
       console.log("login success moving to application gali: was here.");  
       startApp();
@@ -24,7 +24,6 @@ define(['jquery', 'modules/Login/Login', 'core/cookies', 'core/layout','core/win
 		console.log("login fail trying again");
 		login.resetDeferred();
 		login.getPromise().then(loginSuccess, loginFail);
-		
     }
 	
     login = new Login(); //
@@ -35,12 +34,45 @@ define(['jquery', 'modules/Login/Login', 'core/cookies', 'core/layout','core/win
 
   }
   
+   //for sign in new User
+  function startCreate(){
+  
+	  function createSuccess(){
+		console.log("creation success moving to login.");
+		startLoggin();
+		create.destroy();
+	  }
+	  
+	   function createFail(){
+      console.log("creation fail trying again");
+      create.resetDeferred();
+      create.getPromise().then(createSuccess, createFail);
+    }
+	 
+	  
+	create = new Create();
+	create.appendTo("#container");
+	login.destroy();
+	create.getPromise().then(createSuccess,createFail);  
+	  
+	  }
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   function startApp(){
     layout.createLayout("3W", "#container");
-	//list=new ListView();
-	request.upload();
+	user=cookies.getCookie('user');
+	console.log("starting application " +user);
+	//lst=
+	request.upload(user);
+	//console.log(lst);
 	
 //	menu.createMenu("panelBar", "#megaStore");
 	window.createButton("buttonPlus", "#windowButton");
