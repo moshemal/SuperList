34f7bad50ef753eac73422f31beec8f2f60f05f2
galiii <gali.brill.1408@gmail.com>
2015-12-15 16:56:50 +0200

    edit

--- client/modules/list_edit/EditOrRemoveWindow/EditOrRemForm.js
+++ client/modules/list_edit/EditOrRemoveWindow/EditOrRemForm.js
@@ -23,12 +23,13 @@ function EditOrRemForm(initObj){
             }).data("kendoWindow");
 	 
 	 this.$.find("input").val(initObj);
+	 var  newName= this.$.find("input").val();
+			consloe.log(newName);
 			/*for rename the name of the list*/
 			this.$.find("#editform .buttons #btnEdit").on('submit', function(ev){
 			//var newName = ev.target[0].value;
 			//consloe.log(ev.target[0].value;);
-			var  newName= this.$.find("input").val();
-			consloe.log(newName);
+			
 			var promise = request.editList(initObj,newName);
 			promise.then(function(){that._dfd.resolve()}, function(){that._dfd.reject()});
 			return false;
