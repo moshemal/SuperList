ff45af03c491c05b0db4ed6c03c04d03aee2eef3
galiii <gali.brill.1408@gmail.com>
2016-05-16 15:38:31 +0300

    fixinig clealing in apiRoutes

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,13 +1,14 @@
 var requestHandlers = require("./requestHandlers");
 var auth 						= require("./auth");
 var cookies					= require("./cookies");
-var db							= require('./db');
+//var db							= require('./db');
 
 
-//i have problom with the cookies
+
 function decorateWithIsloggedIn(func){
 return function (response, parsedUrl, postData, request){
-		var cks = cookies.parseCookies(request);
+		var cks = cookies.parseCookies(request);//i have problom with the cookies
+		console.log("welcome to decorateWithIsloggedIn line 11 in apiRouter.js\n");
 		if (auth.isLoggedIn(cks)){
 			func(response, parsedUrl, postData, request,auth.getLoggedIn(cks));
 		} else {
@@ -20,14 +21,17 @@ return function (response, parsedUrl, postData, request){
 }
 
 function isLoggedIn(response, parsedUrl, postData, request){
-console.log("welcome to decorateWithIsloggedIn line 23 in apiRouter.js");
 	var cks = cookies.parseCookies(request);
-    if (auth.isLoggedIn(cks)){
-        response.writeHead(200, {"Content-Type": "text/plain"});
-    }
-	else{
+    console.log("welcome to isLoggedIn line 25 in apiRouter.js\n");
+	if (auth.isLoggedIn(cks)){
+        response.writeHead(200, {
+			"Content-Type": "text/plain"
+			});
+    }else{
 	console.log("in line 29 apiRouters ");//parsedUrl=Objecet
-        response.writeHead(401, {"Content-Type": "text/plain"});
+        response.writeHead(401, {
+			"Content-Type": "text/plain"
+			});		
     }
     response.end();
 }
