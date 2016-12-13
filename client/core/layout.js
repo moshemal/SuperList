28104c7030fa05989a61450b1b1fb1f88b034e73
galiii <gali.brill.1408@gmail.com>
2016-12-13 03:28:43 +0200

    style

--- client/core/layout.js
+++ client/core/layout.js
@@ -51,7 +51,7 @@ define(['jquery', 'text!./templates/l3W.html','modules/List/List', 'modules/View
 			panes: [
                             { collapsible: true,size:"90px",resizable:false },//top 1
                             { collapsible: false, size: "470",resizable:true  },//middle 2
-                            { collapsible: false, resizable: true, size: "20px" } //buttom 3 button
+                            { collapsible: false, resizable: false, size: "20px" } //buttom 3 button
                         ]
 			
 		});
